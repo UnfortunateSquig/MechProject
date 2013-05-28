0226486a1040ece16529bf8872ba79a70d1da099
Larry Garrett <lgarrettiii@gmail.com>
2013-05-28 17:37:59 -0400

    Initial Commit

---
+++ VisualStudio/MechProject.Server/MechProject.Server/Application.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ExitGames.Logging.Log4Net;
+using Photon.SocketServer;
+using log4net;
+using log4net.Config;
+
+namespace MechProject.Server 
+{
+    public class Application : ApplicationBase
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (Application));
+
+        private readonly List<MechPeer> _peers;
+
+        public IEnumerable<MechPeer> Peers {get { return _peers; }}
+
+        public Application()
+        {
+            _peers = new List<MechPeer>();
+        }
+
+        public void DestroyPeer(MechPeer peer)
+        {
+            _peers.Remove(peer);
+        }
+
+        protected override PeerBase CreatePeer(InitRequest initRequest)
+        {
+            var peer = new MechPeer(this, initRequest);
+            _peers.Add(peer);
+            return peer;
+        }
+
+        protected override void Setup()
+        {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo(Path.Combine(BinaryPath, "log4net.config")));
+            ExitGames.Logging.LogManager.SetLoggerFactory(Log4NetLoggerFactory.Instance);
+
+            Log.Info("Application Started!");
+        }
+
+        protected override void TearDown()
+        {
+            Log.Info("Application ending.");
+        }
+    }
+}
---
+++ VisualStudio/MechProject.Server/MechProject.Server/MechPeer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Photon.SocketServer;
+using PhotonHostRuntimeInterfaces;
+using log4net;
+
+namespace MechProject.Server
+{
+    public class MechPeer : PeerBase
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof (MechPeer));
+        
+        private readonly Application _application;
+
+        public MechPeer(Application application, InitRequest initRequest) : base(initRequest.Protocol, initRequest.PhotonPeer)
+        {
+            _application = application;
+            Log.InfoFormat("Peer created at {0}:{1}", initRequest.RemoteIP, initRequest.RemotePort);
+        }
+
+        protected override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters)
+        {
+            if (operationRequest.OperationCode != 1)
+            {
+                Log.WarnFormat("Peer sent unknown opcode: {0}", operationRequest.OperationCode);
+                return;
+            }
+
+            var message = (string) operationRequest.Parameters[0];
+            Log.DebugFormat("Got message from client: {0}", message);
+
+            var eventData = new EventData(0, new Dictionary<byte, object> {{0, message}});
+            var parameters = new SendParameters() {Unreliable = false};
+
+            foreach (var peer in _application.Peers.Where(t => t != this))
+            {
+                peer.SendEvent(eventData, parameters);
+            }
+        }
+
+        protected override void OnDisconnect(DisconnectReason reasonCode, string reasonDetail)
+        {
+            Log.InfoFormat("Peer Disconnected: {0}, {1}", reasonCode, reasonDetail);
+            _application.DestroyPeer(this);
+        }
+    }
+} 
